ab9f7c69b2acddf483b0b375662c363891e397f9
himekuri <himekuri9@gmail.com>
2021-01-19 22:31:17 +0000

    add column

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -10,6 +10,7 @@
             <thead>
                 <tr>
                     <th>id</th>
+                    <th>タイトル</th>
                     <th>メッセージ</th>
                 </tr>
             </thead>
@@ -17,6 +18,7 @@
                 @foreach ($messages as $message)
                 <tr>
                     <td>{!! link_to_route('messages.show', $message->id, ['message' => $message->id]) !!}</td>
+                    <td>{{ $message->title }}</td>
                     <td>{{ $message->content }}</td>
                 </tr>
                 @endforeach
